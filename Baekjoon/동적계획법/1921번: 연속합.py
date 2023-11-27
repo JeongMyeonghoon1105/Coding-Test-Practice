b1217c6f8f17c25220acccaf1123498604973e10
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2023-11-27 16:18:02 +0900

    동적계획법

--- Baekjoon/동적계획법/1921번: 연속합.py
+++ Baekjoon/동적계획법/1921번: 연속합.py
@@ -1,22 +1,63 @@
-memo = []
+# memo = []
 
-def f(s, i, sum):
-  if i >= len(s):
-    return
-  sum += s[i]
-  memo.append(sum)
-  memo.append(f(s, i+1, 0))
-  memo.append(f(s, i+1, sum))
-  return
+# # O(2^n) 풀이
+# # def f(s, i, sum):
+# #   if i >= len(s):
+# #     return
+# #   sum += s[i]
+# #   memo.append(sum)
+# #   memo.append(f(s, i+1, 0))
+# #   memo.append(f(s, i+1, sum))
+# #   return
 
-n = int(input())
-series = list(map(int, input().split()))
-f(series, 0, 0)
-# print(memo)
-# print(max(memo))
-m = memo[0]
-for i in memo:
-  if not (i == None):
-    if m < i:
-      m = i
-print(m)
+# # O(n^2) 풀이
+# # def f(s):
+# #   start = 0
+# #   end = 0
+# #   while start < len(s):
+# #     end = start
+# #     while end < len(s):
+# #       sum = 0
+# #       for i in range(start, end+1):
+# #         sum += s[i]
+# #       memo.append(sum)
+# #       end += 1
+# #     start += 1
+# #   print(max(memo))
+
+# # ??? 풀이
+# # def f(s, start, end):
+# #   if start == end:
+# #     memo.append(s[start])
+# #   else:
+# #     memo.append(f(s, start, end-1)+s[end])
+# #   return memo[-1]
+
+# n = int(input())
+# series = list(map(int, input().split()))
+
+# start = 0
+# end = 0
+# while end < len(series):
+#   sum = 0
+#   for i in range(start, end+1):
+#     sum += series[i]
+#   memo.append(sum)
+#   end += 1
+
+# # f(series, 0, n-1)
+# s = series[0]
+# index = 1
+# for stage in range(1, n):
+#   for j in range(n-stage):
+#     memo.append(memo[index+j]-series[stage-1])
+#   s += series[stage]
+#   index += (n+1-stage)
+#   # print(index) 
+#   # print(memo)
+
+# # for i in range(n):
+# #   f(series, i, n-1)
+# print(max(memo))  
+
+
