7f292438fa5a19b2f0a13b12f97e234a3bb59f6d
unknown <wjdaudgns1105@naver.com>
2023-12-29 17:27:44 +0900

    For pull

--- 기초 연습/피보나치수열_bottom_up.c
+++ 기초 연습/피보나치수열_bottom_up.c
@@ -10,7 +10,7 @@ int f(int n) {
     else if (n > LIMIT)
         fprintf(stderr, "Out of range.\n");
         return -1;
-    for (int i = 1; i <= n; i++)
+    for (int i = 2; i <= n; i++)
         memo[i] = memo[i-1] + memo[i-2];
     return memo[n];
 }
