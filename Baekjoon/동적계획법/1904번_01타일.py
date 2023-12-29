366b282dc0db86467d03817555e99cf14a6aa6e7
Your Name <you@example.com>
2023-12-29 17:18:09 +0900

    BOJ no.1904

--- Baekjoon/동적계획법/1904번_01타일.py
+++ Baekjoon/동적계획법/1904번_01타일.py
@@ -1,11 +1,11 @@
-memo = [False, 1, 2]
+memo = [0, 1, 2]
 
 def f(n):
     if n <= 2:
         return n
-    memo.append(f(n-1)+f(n-2))
+    for i in range(3, n+1):
+        memo.append((memo[i-1] + memo[i-2]) % 15746)
     return memo[n]
 
-n = int(input())
-f(n)
-print(memo[n] % 15746)
+N = int(input())
+print(f(N))
