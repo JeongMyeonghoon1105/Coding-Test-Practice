0662803faa8d6feeed057578074530f39d6888b6
Your Name <you@example.com>
2024-01-05 17:35:18 +0900

    Heap

--- Baekjoon/트리/1927번_최소_힙.c
+++ Baekjoon/트리/1927번_최소_힙.c
@@ -1,34 +1,95 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-#define N 100000
+#define N 100001
 
 typedef int element;
 
 typedef struct {
-    int head;
+    int rear;
     element heap[N];
 } HeapType;
 
 void init(HeapType *H) {
-    H->head = 0;
+    H->rear = 1;
     for (int i = 0; i < N; i++)
         H->heap[i] = -1;
 }
 
-void insert() {
-    
+void insert(HeapType *H, element e) {
+    H->heap[H->rear] = e;
+    int child = H->rear;
+    if (!(child)) {
+        int parent = child / 2, temp;
+        while (parent >= 1 && H->heap[parent] > H->heap[child]) {
+            temp = H->heap[parent];
+            H->heap[parent] = H->heap[child];
+            H->heap[child] = temp;
+            child = parent;
+            parent = child / 2;
+        }
+    }
+    H->rear++;
 }
 
-void delete() {
+element delete(HeapType *H) {
+    if (H->rear == 1)
+        return 0;
+    int e = H->heap[0];
+    H->heap[0] = H->heap[H->rear];
+    H->heap[H->rear] = -1;
+    H->rear--;
 
+    int parent = 1, lc = 2, rc = 3, temp;
+    while (parent < H->rear && (H->heap[parent] < H->heap[lc] || H->heap[parent] < H->heap[rc])) {
+        lc = parent * 2, rc = parent * 2 + 1;
+        if (H->heap[parent] < H->heap[lc] && H->heap[parent] < H->heap[rc]) {
+            if (H->heap[lc] > H->heap[rc]) {
+                temp = H->heap[parent];
+                H->heap[parent] = H->heap[lc];
+                H->heap[lc] = temp;
+                parent = lc;
+            } else {
+                temp = H->heap[parent];
+                H->heap[parent] = H->heap[rc];
+                H->heap[rc] = temp;
+                parent = rc;
+            }
+        } else if (H->heap[parent] < H->heap[lc]) {
+            temp = H->heap[parent];
+            H->heap[parent] = H->heap[lc];
+            H->heap[lc] = temp;
+            parent = lc;
+        } else if (H->heap[parent] < H->heap[rc]) {
+            temp = H->heap[parent];
+            H->heap[parent] = H->heap[rc];
+            H->heap[rc] = temp;
+            parent = rc;
+        }
+    }
+
+    return e;
+}
+
+void print(HeapType *H) {
+    for (int i = 1; i < H->rear; i++)
+        printf("%d ", H->heap[i]);
+    printf("\n");
 }
 
 int main() {
-    HeapType *h;
+    HeapType h;
     init(&h);
 
-    int n;  scanf("%d", &n);
+    int n, inp;  scanf("%d", &n);
     for (int i = 0; i < n; i++) {
-
+        scanf("%d", &inp);
+        
+        if (inp)
+            insert(&h, inp);
+        else
+            printf("%d\n", delete(&h));
+        
+        print(&h);
     }
 }
